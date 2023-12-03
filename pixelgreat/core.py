edd1daee7908a9e24c1c7e6af5be75b5174fa3f9
Ella Jameson <ellagjameson@gmail.com>
2023-12-03 00:33:59 -0700

    more init

--- pixelgreat/core.py
+++ pixelgreat/core.py
@@ -8,60 +8,7 @@ from . import helpers
 
 # ---- MAIN CLASSES ----
 
-# Main class
-class Python3Template:
-    def __init__(self,
-                 first,
-                 second,
-                 opcode
-                 ):
-        self.first = first
-        self.second = second
-        self.opcode = opcode.strip()
-        
-        if self.opcode not in self.OpCodes.VALID_OPTIONS.value:
-            raise argparse.ArgumentTypeError("The operation code must be either \"+\", \"-\", \"*\", or \"/\"")
-        if self.opcode == self.OpCodes.DIV.value and self.second == 0:
-            raise ZeroDivisionError("Cannot divide by zero!")
 
-    class OpCodes(Enum):
-        ADD = "+"
-        SUB = "-"
-        MULT = "*"
-        DIV = "/"
-        VALID_OPTIONS = "+-*/"
-    
-    def add(self):
-        return self.first + self.second
-    
-    def sub(self):
-        return self.first - self.second
-    
-    def mult(self):
-        return self.first * self.second
-    
-    def div(self):
-        return self.first / self.second
-    
-    def operate(self):
-        if self.opcode == self.OpCodes.ADD.value:
-            print(f"The equation is {self.first} + {self.second}")
-            return self.add()
-        elif self.opcode == self.OpCodes.SUB.value:
-            print(f"The equation is {self.first} - {self.second}")
-            return self.sub()
-        elif self.opcode == self.OpCodes.MULT.value:
-            print(f"The equation is {self.first} * {self.second}")
-            return self.mult()
-        elif self.opcode == self.OpCodes.DIV.value:
-            print(f"The equation is {self.first} / {self.second}")
-            return self.div()
-        else:
-            raise argparse.ArgumentTypeError(f"Invalid opcode: \"{self.opcode}\"")
-    
-    def run(self):
-        result = self.operate()
-        print(f"The answer is {result}")
 
 
 # ---- PROGRAM EXECUTION ----
@@ -83,15 +30,10 @@ def parse_args(args):
     return parser.parse_args()
 
 
-def main(args):
-    args = parse_args(args)
+def main(raw_args):
+    args = parse_args(raw_args)
     
-    python_3_template = Python3Template(
-        first=args.first_arg, 
-        second=args.second_arg, 
-        opcode=args.opcode)
-    
-    python_3_template.run()
+    print(args)
 
 
 def run():
