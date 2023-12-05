1dcf6ccb9c46f5cfb2a8a6293f6a77f611f8a0d2
Ella Jameson <ellagjameson@gmail.com>
2023-12-04 20:57:23 -0700

    Update core.py

--- pixelgreat/core.py
+++ pixelgreat/core.py
@@ -12,43 +12,7 @@ from . import filters
 # ---- MAIN CLASSES ----
 
 
-class LcdPixelate:
-    def __init__(self,
-                 pixel_size,
-                 pixel_aspect=1.0,
-                 pixel_padding=0.25,
-                 pixel_rounding=0.25,
-                 pixel_direction=Direction.VERTICAL,
-                 overlay_only=False,
-                 blur=0.0,
-                 blur_ratio=1.0,
-                 filter_strength=1.0,
-                 bloom_strength=0.5
-                 ):
-        if pixel_size < 3:
-            raise ValueError("Pixel size must be at least 3")
-        self.pixel_size = pixel_size
 
-        self.pixel_aspect = max(abs(pixel_aspect), 0.01)
-
-        self.pixel_padding = helpers.clip(pixel_padding, 0, 1)
-
-        self.pixel_rounding = helpers.clip(pixel_rounding, 0, 1)
-
-        self.overlay_only = overlay_only
-
-        self.pixel_direction = pixel_direction
-
-        self.blur = helpers.clip(blur, 0, 1)
-
-        self.blur_ratio = helpers.clip(blur_ratio, 0, 1)
-
-        self.filter_strength = helpers.clip(filter_strength, 0, 1)
-
-        self.bloom_strength = helpers.clip(bloom_strength, 0, 1)
-
-    def process(self, image):
-        pass
 
 
 # ---- PROGRAM EXECUTION ----
@@ -76,8 +40,7 @@ def parse_args(args):
 def main(raw_args):
     args = parse_args(raw_args)
 
-    test = LcdPixelate(6)
-    test.process("Wow")
+    pass
 
 
 def run():
